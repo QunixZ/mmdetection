f15426f0d0514f63e37d2b60905d519bc8c147b8
qunixz <zeng_qunqi@163.com>
2020-11-17 10:43:31 +0800

    add demo_runner

---
+++ demo/run_image_demo.sh
@@ -0,0 +1,2 @@
+python demo/image_demo.py demo/demo.jpg configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth
+#must modify the mmdet/apis/inference.py to allow save result in png files
